9a8d01263d43453b57a32d6104ffadcacb624947
Mary <137616139+Maria-Caroline@users.noreply.github.com>
2025-06-17 15:52:54 -0300

    Update parseParagraph.tsx

--- john-singer-sargent-memoir/src/app/utils/parseParagraph.tsx
+++ john-singer-sargent-memoir/src/app/utils/parseParagraph.tsx
@@ -2,34 +2,90 @@ import React from 'react';
 import { ModalInfo, ModalInfoProps } from '../[locale]/components/modal-info';
 
 const infoWords: Record<string, Omit<ModalInfoProps, 'description'>> = {
-  technique: {
-    word: 'technique',
-    image: '/modals/technique.png',
-  },
-  portrait: {
-    word: 'portrait',
-    image: '/modals/portrait.png',
-  },
-  landscape: {
-    word: 'landscape',
-    image: '/modals/landscape.png',
-  },
-  oil: {
-    word: 'oil',
-    image: '/modals/master.png',
-  },
-  watercolors: {
-    word: 'watercolors',
-    image: '/modals/master.png',
-  },
-  charcoal: {
-    word: 'charcoal',
-    image: '/modals/master.png',
-  },
-  Impressionist: {
-    word: 'Impressionist',
-    image: '/modals/master.png',
-  },
+    technique: {
+        word: 'technique',
+        image: '/modals/technique.png',
+    },
+    portrait: {
+        word: 'portrait',
+        image: '/modals/portrait.png',
+    },
+    portraiture: {
+        word: 'portraiture',
+        image: '/modals/portrait.png',
+    },
+    landscape: {
+        word: 'landscape',
+        image: '/modals/landscape.png',
+    },
+    paysages: {
+        word: 'paysages',
+        image: '/modals/landscape.png',
+    },
+    oil: {
+        word: 'oil',
+        image: '/modals/oil.png',
+    },
+    huile: {
+        word: 'oil',
+        image: '/modals/oil.png',
+    },
+    watercolors: {
+        word: 'watercolors',
+        image: '/modals/wtc.png',
+    },
+    aquarelles: {
+        word: 'aquarelles',
+        image: '/modals/wtc.png',
+    },
+    aquarelle: {
+        word: 'aquarelles',
+        image: '/modals/wtc.png',
+    },
+    charcoal: {
+        word: 'charcoal',
+        image: '/modals/master.png',
+    },
+    charbon: {
+        word: 'charbon',
+        image: '/modals/master.png',
+    },
+    Impressionist: {
+        word: 'Impressionist',
+        image: '/modals/master.png',
+    },
+    esquisses: {
+        word: 'esquisses',
+        image: '/modals/skt.png',
+    },
+    sketches: {
+        word: 'sketches',
+        image: '/modals/skt.png',
+    },
+    perspective: {
+        word: 'perspective',
+        image: '/modals/master.png',
+    },
+    anatomie: {
+        word: 'anatomie',
+        image: '/modals/master.png',
+    },
+    anatomy: {
+        word: 'anatomy',
+        image: '/modals/skt.png',
+    },
+    carnets: {
+        word: 'carnets',
+        image: '/modals/skt.png',
+    },
+    sketchbooks: {
+        word: 'carnets',
+        image: '/modals/skt.png',
+    },
+    impressionnistes: {
+        word: 'carnets',
+        image: '/modals/skt.png',
+    },
 };
 
 export function parseParagraph(text: string, t: (key: string) => string): React.ReactNode[] {
